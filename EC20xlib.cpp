921883e517b2f096e8a2a9760d612b0d728cfc28
luyang14 <luyang1435097637@gmail>
2022-01-05 17:14:51 +0800

    add tcp connect

--- EC20xlib.cpp
+++ EC20xlib.cpp
@@ -21,7 +21,7 @@ EC20xlib::EC20xlib(Stream &serial) {
 #else
     EC20xconn = &serial;
     
-    EC20xconn->setTimeout(100);//阻塞时间
+    EC20xconn->setTimeout(50);//阻塞时间
 #endif
 }
 
@@ -218,7 +218,7 @@ byte EC20xlib::EC20xcommand(const char *command, const char *resp1, const char *
 
     // Get rid of any buffered output.
     EC20xconn->flush();
-
+    //loop();
     while (count < repetitions && returnValue != EC20x_OK) {
         log("Issuing command: ");
         logln(command);
@@ -235,12 +235,47 @@ byte EC20xlib::EC20xcommand(const char *command, const char *resp1, const char *
     }
     return returnValue;
 }
+/****************************************
+* 函数名：EC20_SendCommand
+* 描述  ：向GPRS模块发送命令
+*         CommandStr 命令字符串
+*         Check 检测的命令回复
+*         retry 重试次数
+*         每次等待命令回复的时间 单位100mS
+* 输入  ：无
+* 输出  ：无
+****************************************/
+byte EC20xlib::EC20xcommand(const char *command,uint16_t commandlen, const char *resp1, const char *resp2, int timeout, int repetitions, String *response)
+{
+    byte returnValue = EC20x_NOTOK;
+    byte count = 0;
+    resend_state = STSTE_SEND;
+    // Get rid of any buffered output.
+    EC20xconn->flush();
 
+    while (count < repetitions && returnValue != EC20x_OK) {
+        log("Issuing command: ");
+        logln(command);
+
+        EC20xconn->write(command,commandlen);
+        EC20xconn->write('\r');
+
+        if (EC20xwaitFor(resp1, resp2, timeout, response) == EC20x_OK) {
+            returnValue = EC20x_OK;
+        } else {
+            returnValue = EC20x_NOTOK;
+        }
+        count++;
+    }
+    resend_state = STSTE_RE;
+    return returnValue;
+}
 
 // Wait for responses.
 byte EC20xlib::EC20xwaitFor(const char *resp1, const char *resp2, int timeout, String *response) {
     unsigned long entry = millis();
     String reply = "";
+    String reply2 = "";
     byte retVal = 99;
     do {
         reply += read();
@@ -272,7 +307,47 @@ byte EC20xlib::EC20xwaitFor(const char *resp1, const char *resp2, int timeout, S
             retVal = EC20x_NOTOK;
         }
     }
+    reply2 = reply;
+    if(reply2.indexOf("+QIURC: \"recv\",") != -1)//接收到数据
+    {
+        logln("recv data");
+        uint8_t connectID = 0;
+        uint16_t datalen = 0;
+        char sendbuf[1000] = "";
+        int commaPosition = 0;
+        commaPosition = reply2.indexOf("+QIURC: \"recv\",");
+        reply2 = reply2.substring(commaPosition,reply2.length());//截取有效数据
+
+        for(uint8_t i =0;i<3;i++){
+            commaPosition = reply2.indexOf(",");
+            if(commaPosition != -1){//检测到“，”
+                if(i == 1){
+                    connectID = atoi(reply2.substring(0,commaPosition).c_str());
+                    log("connectID:");
+                    logln(connectID);
+                    datalen = atoi(reply2.substring(commaPosition+1,reply2.indexOf("\r")).c_str());
+                    logln("datalen---------------------------------");
+                    logln(datalen);
+                    break;
+                }
+                reply2 = reply2.substring(commaPosition+1,reply2.length());
+            }
+        }
+        commaPosition = reply2.indexOf("HTTP/1.1");
+        if(commaPosition != -1){//检测到“HTTP/1.1”
+            reply2 = reply2.substring(commaPosition,reply2.length());
+            reply2.toCharArray(sendbuf,reply2.length()+1,0);
+            getdatecallback(sendbuf,datalen,connectID);
+        }
+    }
     return retVal;
+}
+
+void EC20xlib::attachsubcallback(subcallbackFunction newFunction){
+    subdatecallback = newFunction;
+}
+void EC20xlib::attachtcpcallback(recallbackFunction newFunction){
+    getdatecallback = newFunction;
 }
 
 // Read some data from the EC20x in a non-blocking manner.
@@ -401,7 +476,7 @@ byte EC20xlib::getMessage(char *topic,char* msg,uint8_t qos1)//typ_NEC_AQData  *
         log((millis() - entry));
         log(" ms: ");
         logln(reply);
-        if(reply.indexOf("+QMTRECV:") != -1)
+        if(reply.indexOf("+QIURC:") != -1)//接收到服务器返回
         {
             logln("recv data");
             uint8_t i = 0;
@@ -472,4 +547,158 @@ byte EC20xlib::getMessage(char *topic,char* msg,uint8_t qos1)//typ_NEC_AQData  *
         
     }
     return retVal;
+}
+
+/*
+TCP连接服务器
+*/
+int EC20xlib::disconnect(uint8_t connectID)
+{
+    char sdata[30];//格式化AT指令时使用
+    uint8_t retVal = EC20x_NOTOK;
+    sprintf(sdata,"AT+QICLOSE=%d",connectID);
+    if(EC20xcommand(sdata,"OK","yy",EC20x_CMD_TIMEOUT,2,NULL)!=0)
+        retVal = EC20x_FAILURE;
+    else retVal = EC20x_OK;
+
+    return retVal;
+}
+/*
+TCP连接服务器
+*/
+int EC20xlib::connect(char *ip, char *port,uint8_t connectID)
+{
+    char data_buff[100];//格式化AT指令时使用
+    char sdata[30];//格式化AT指令时使用
+    uint8_t retVal = EC20x_NOTOK;
+    String reply = "";
+    logln("creat a socket");
+    //判断是否连接服务器
+    sprintf(data_buff,"AT+QIOPEN=1,%d,\"TCP\",\"%s\",%s,0,1",connectID,ip,port);
+    sprintf(sdata,"+QIOPEN: %d,",connectID);
+    if(EC20xcommand(data_buff,sdata,"yy",EC20x_CMD_TIMEOUT+3000,1,&reply) == 0)
+    {
+        memset(sdata,0,sizeof(sdata));
+        sprintf(sdata,"+QIOPEN: %d,0",connectID);
+        if(reply.indexOf(sdata) != -1)
+        {
+            logln("connected server successfully");
+            return EC20x_OK;
+        }
+        else //连接失败
+        {
+            logln("send failed,close the socket!!");
+
+            if(disconnect(connectID)!=0)
+                retVal = EC20x_FAILURE;
+            memset(sdata,0,sizeof(sdata));
+            sprintf(sdata,"+QIOPEN: %d,0",connectID);
+            if(EC20xcommand(data_buff,sdata,"yy",EC20x_CMD_TIMEOUT,2,NULL) != 0)
+                retVal = EC20x_FAILURE;
+            else
+                retVal = EC20x_OK;
+        }
+    }
+    return retVal;
+}
+
+/************************************************
+*函数名： EC20_senddata
+*功能  ： 发送AQ
+*参数  ： buf     数据
+*         buflen  数据包长度
+*输入  ： 无
+*输出  ： 无
+************************************************/
+int EC20xlib::postdata(char* buf, int buflen,uint8_t connectID)
+{
+    uint8_t retVal = EC20x_NOTOK;
+    char data_buff[30];//格式化AT指令时使用
+    
+    // 喂狗操作，防止信号问题引起死机
+//    IWDG_Feed();
+
+    //获取数据包长度，合并成字符串
+    sprintf(data_buff,"AT+QISEND=%d,%d\r\n",connectID,buflen);
+
+    if(EC20x_OK == EC20xcommand(data_buff,strlen(data_buff),">","yy",EC20x_CMD_TIMEOUT+3000,1,NULL)){
+        //发送数据
+        if(EC20x_OK == EC20xcommand(buf,buflen,"SEND OK","yy",EC20x_CMD_TIMEOUT+3000,1,NULL))
+            retVal = EC20x_OK;
+        else retVal = EC20x_FAILURE;
+    }else retVal = EC20x_FAILURE;
+    return retVal;
+}
+/*
+发布主题
+*/
+void EC20xlib::loop()//typ_NEC_AQData  *AQData_Send,
+{  
+    //    uint16_t i=0;
+    uint32_t entry = millis();
+    int commaPosition = 0;//存储还没有分离出来的字符串
+    String reply = "";
+
+    if(resend_state == STSTE_RE){//发送完成，可以接收
+        reply = read();
+#ifdef ESP32
+        yield();
+#endif
+        if(reply != "") {
+            log("Reply in ");
+            log((millis() - entry));
+            log(" ms: ");
+            logln(reply);
+            /*示例
+            +QIURC: "recv",0,256
+            HTTP/1.1 200 
+            Content-Type: application/json;charset=UTF-8
+            Transfer-Encoding: chunked
+            Vary: Accept-Encoding
+            Date: Wed, 05 Jan 2022 02:27:40 GMT
+
+            63
+            {"pulseAll":4997520,"dateStr":"2022-01-05 10:27:40","pulseMoney":4997520,"state":"1","pulseFree":0}
+            */
+            if(reply.indexOf("+QIURC: \"recv\",") != -1)//接收到数据
+            {
+                logln("recv data");
+                uint8_t connectID = 0;
+                uint16_t datalen = 0;
+                char sendbuf[1000] = "";
+                commaPosition = reply.indexOf("+QIURC: \"recv\",");
+                reply = reply.substring(commaPosition,reply.length());//截取有效数据
+
+                for(uint8_t i =0;i<3;i++){
+                    commaPosition = reply.indexOf(",");
+                    if(commaPosition != -1){//检测到“，”
+                        if(i == 1){
+                            connectID = atoi(reply.substring(0,commaPosition).c_str());
+                            log("connectID:");
+                            logln(connectID);
+                            datalen = atoi(reply.substring(commaPosition+1,reply.indexOf("\r")).c_str());
+                            logln("datalen---------------------------------");
+                            logln(datalen);
+                            break;
+                        }
+                        reply = reply.substring(commaPosition+1,reply.length());
+                    }
+                }
+                commaPosition = reply.indexOf("HTTP/1.1");
+                if(commaPosition != -1){//检测到“HTTP/1.1”
+                    reply = reply.substring(commaPosition,reply.length());
+                    reply.toCharArray(sendbuf,reply.length()+1,0);
+                    getdatecallback(sendbuf,datalen,connectID);
+                }
+            }
+            if(reply.indexOf("+QIURC: \"closed\",") != -1)//断开连接
+            {
+                logln("断开连接");
+            }
+            if(reply.indexOf("+QMTRECV:") != -1)
+            {
+            }    
+
+        }
+    }
 }
--- EC20xlib.h
+++ EC20xlib.h
@@ -3,7 +3,7 @@
 
 #include <Arduino.h>
 
-#define DEBUG
+//#define DEBUG
 #ifdef DEBUG
 #define log(msg) Serial.print(msg)
 #define logln(msg) Serial.println(msg)
@@ -11,6 +11,10 @@
 #define log(msg)
 #define logln(msg)
 #endif
+
+#define USE_HTTP 1
+#define USE_MQTT 1
+#define USE_TCP  1
 
 #define countof(a) (sizeof(a) / sizeof(a[0]))
 
@@ -22,6 +26,13 @@
 
 #define EC20x_CMD_TIMEOUT 2000
 
+#define STSTE_SEND 1
+#define STSTE_RE   0
+
+extern "C" {
+typedef byte (*recallbackFunction)(char* buf, int buflen,uint8_t connectID);
+typedef byte (*subcallbackFunction)(char *topic,char* msg,int buflen,uint8_t qos1);
+}
 //#define EC20xconn Serial2
 
 class EC20xlib {
@@ -31,7 +42,7 @@ public:
 
     byte begin();
     byte blockUntilReady(long baudRate);
-
+    
     void powerCycle(int pin);
     void powerOn(int pin);
     void powerOff(int pin);
@@ -39,6 +50,10 @@ public:
     int getSignalStrength();
     String getRealTimeClock();
     
+    int connect(char *ip, char *port,uint8_t connectID);
+    int disconnect(uint8_t connectID);
+    uint8_t connected();
+    int  postdata(char* buf, int buflen,uint8_t connectID);
     byte connectMqttServer(char *clientID,char *ip,char *port,char *username,char *password);
     byte SubTopic(char *topic,uint8_t qos1);
     byte PubTopic(char *topic,char* msg,uint8_t qos1);
@@ -46,12 +61,21 @@ public:
     byte getMessage(char *topic,char* msg,uint8_t qos1);
     byte Read_CCID(char *DataBuf);
     byte Read_IMEI(char *DataBuf);
+    void attachsubcallback(subcallbackFunction newFunction);
+    void attachtcpcallback(recallbackFunction newFunction);
+    void loop();
 private:
     Stream* EC20xconn;
+    uint8_t resend_state;
+    char *ip;
+    char *port;
     String read();
     byte EC20xcommand(const char *command, const char *resp1, const char *resp2, int timeout, int repetitions, String *response);
+    byte EC20xcommand(const char *command,uint16_t commandlen, const char *resp1, const char *resp2, int timeout, int repetitions, String *response);
     byte EC20xwaitFor(const char *resp1, const char *resp2, int timeout, String *response);
     long detectRate();
     char setRate(long baudRate);
+    recallbackFunction getdatecallback = NULL;
+    subcallbackFunction subdatecallback = NULL;
 };
 #endif
